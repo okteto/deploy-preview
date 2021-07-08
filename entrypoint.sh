904177edf6f33ae6b609f041f452c82aa6697fe9
Javier López Barba <javier@okteto.com>
2021-07-08 12:54:21 +0200

    Add support forcustom certificates
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,12 @@ set -e
 name=$1
 type=$2
 
+if [[ ! -z "$OKTETO_CA_CERT" ]]; then
+   echo "Custom certificate is provided"
+   echo "$OKTETO_CA_CERT" > /usr/local/share/ca-certificates/okteto_ca_cert
+   update-ca-certificates
+fi
+
 if [ -z $name ]; then
   echo "Preview environment name is required"
   exit 1
