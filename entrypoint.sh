46df029727b6b8528a0fee685dda27098e6804e7
Javier López Barba <javier@okteto.com>
2021-07-21 18:36:07 +0200

    Add sourceUrl
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,7 +52,10 @@ if [ ! -z "${variables}" ]; then
 fi
 
 export OKTETO_DISABLE_SPINNER=1
-
+number=$(jq '[ .number ][0]' event.json) 
+echo running: okteto preview deploy $name -scope $scope --branch="${branch}" --repository="${GITHUB_SERVER_URL}/${repository}}" --sourceUrl="${GITHUB_SERVER_URL}/${repository}/pull/${number}" ${params} --wait
+okteto preview deploy $name --scope $scope --branch="${branch}" --repository="${GITHUB_SERVER_URL}/${repository}" --sourceUrl="${GITHUB_SERVER_URL}/${repository}/pull/${number}" ${params} --wait
+ 
 echo running: okteto preview deploy $name -scope $scope --branch="${branch}" --repository="${GITHUB_SERVER_URL}/${repository}" ${params} --wait
 okteto preview deploy $name --scope $scope --branch="${branch}" --repository="${GITHUB_SERVER_URL}/${repository}" ${params} --wait
  
