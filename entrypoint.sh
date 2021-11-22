3ee18d3dcd3ff28e2b2b679b21fbd780bd0329fe
Pablo Chico de Guzman <pchico83@gmail.com>
2021-11-22 22:45:55 +0100

    Add debug logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,9 +52,9 @@ fi
 
 export OKTETO_DISABLE_SPINNER=1
 number=$(jq '[ .number ][0]' $GITHUB_EVENT_PATH)
-echo running: okteto preview deploy $name -scope $scope --branch="${branch}" --repository="${GITHUB_SERVER_URL}/${repository}" --sourceUrl="${GITHUB_SERVER_URL}/${repository}/pull/${number}" ${params} --wait
+echo running: okteto preview deploy $name --scope $scope --branch="${branch}" --repository="${GITHUB_SERVER_URL}/${repository}" --sourceUrl="${GITHUB_SERVER_URL}/${repository}/pull/${number}" ${params} --wait
 ret=0
-okteto preview deploy $name --scope $scope --branch="${branch}" --repository="${GITHUB_SERVER_URL}/${repository}" --sourceUrl="${GITHUB_SERVER_URL}/${repository}/pull/${number}" ${params} --wait || ret=1
+okteto preview deploy $name --scope $scope --branch="${branch}" --repository="${GITHUB_SERVER_URL}/${repository}" --sourceUrl="${GITHUB_SERVER_URL}/${repository}/pull/${number}" ${params} --wait --loglevel=debug || ret=1
 
 if [ ! -z $GITHUB_TOKEN ]; then
   withErrors="preview deployed with resource errors"
